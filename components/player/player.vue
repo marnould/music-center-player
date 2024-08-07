97a9fe595cdf1d813766357eed1f2c09e53bd900
marnould <marnould.contact@gmail.com>
2024-08-07 19:25:30 +0700

    init

---
+++ components/player/player.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="mt-5">
+    <table class="table">
+      <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Title</th>
+        <th scope="col">Artist</th>
+        <th scope="col">Date created</th>
+        <th scope="col">Action</th>
+      </tr>
+      </thead>
+      <div
+        v-if="tracksLoading"
+        class="spinner-border"
+        style="width: 3rem; height: 3rem;"
+        role="status"
+      >
+        <span class="sr-only">Loading...</span>
+      </div>
+      <tbody v-else>
+      <tr v-for="(track, index) in tracks" :key="index">
+        <td>{{ index + 1 }}</td>
+        <td>{{ track.title }}</td>
+        <td>{{ track.artist }}</td>
+        <td>{{ track.created }}</td>
+        <td>
+          <button class="btn btn-info" @click="deleteTrack(track._id)">Delete</button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+
+  </section>
+</template>
---
+++ pages/player/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <player />
+  </div>
+</template>
+<script>
+import player from '@/components/player/player'
+export default {
+  components: {
+    player
+  }
+}
+</script>
+
+tracks = [];
+tracksLoading: false,
+
+
+async getTracks() {
+this.tracksLoading = true
+try {
+let data = await this.$axios.$get('/tracks')
+this.tracks = data
+this.tracksLoading = false
+} catch (err) {
+this.tracksLoading = false
+swal('Error', 'Error Fetching Tracks', 'error')
+}
+}
+
+created() {
+this.getTracks()
+}
